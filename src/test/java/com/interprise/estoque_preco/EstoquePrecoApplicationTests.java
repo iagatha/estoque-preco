e77b829c7b771418fefd3e95acdfbfca5990baa6
iagatha <iagathasouzaa@gmail.com>
2025-08-12 11:38:49 -0300

    projeto inicial

---
+++ src/test/java/com/interprise/estoque_preco/EstoquePrecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interprise.estoque_preco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoquePrecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
